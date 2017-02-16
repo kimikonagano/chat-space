08b3f35f1c3c0386a5f9394b320c62ad0f5a7cc4
Nagano Kimiko <kimikonaganosw@gmail.com>
2017-02-16 15:19:55 +0900

    developed message function 3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,6 +11,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path
+      flash[:success] = "メッセージ送信完了です(´･ω･`)"
     else
       redirect_to group_messages_path
       flash[:alert] = "メッセージを入力してください。"
