e88d254a26ea6ead00af821c0e20c7655f5fda2f
Nagano Kimiko <kimikonaganosw@gmail.com>
2017-02-19 10:42:35 +0900

    model_test 2

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
-    it "is invalid without a message" do
-    message = build(:message, text: "")
-    message.valid?
-    expect(message.errors[:text]).to include("can't be blank")
+    it "is valid with a message" do
+    message = build(:message, text: "hmhm")
+    expect(message).to be_valid
     end
   end
 
   describe '#create' do
-    it "is valid with a message" do
-    message = build(:message, text: "hmhm")
-    expect(message).to be_valid
+    it "is invalid without a message" do
+    message = build(:message, text: "")
+    message.valid?
+    expect(message.errors[:text]).to include("can't be blank")
     end
   end
 end
